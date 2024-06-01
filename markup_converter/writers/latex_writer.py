9233f692685483117b91b25c395044bfeb3cd735
Nexooon <hubert.brzoskniewicz@gmail.com>
2024-06-01 22:35:06 +0200

    latex writer

---
+++ markup_converter/writers/latex_writer.py
@@ -0,0 +1,132 @@
+from .writer import Writer
+from ..structure.document import Document
+from .. import structure as st
+
+
+class LatexWriter(Writer):
+    """A class representing a Latex writer for a document.
+
+    :param input: The document to write.
+    :type input: Document
+    """
+    def __init__(self, input: Document) -> None:
+        super().__init__(input)
+
+    def convert_space(self, obj: st.Inline.Space) -> str:
+        return " "
+
+    def convert_soft_break(self, obj: st.Inline.SoftBreak) -> str:
+        return "\\"
+
+    def convert_line_break(self, obj: st.Inline.LineBreak) -> str:
+        return "\\par"
+
+    def convert_str(self, obj: st.Inline.Str) -> str:
+        return obj.content
+
+    def convert_emph(self, obj: st.Inline.Emph) -> str:
+        return f"\\emph{{{self.convert_element(obj.content)}}}"
+
+    def convert_underline(self, obj: st.Inline.Underline) -> str:
+        return f"\\underline{{{self.convert_element(obj.content)}}}"
+
+    def convert_strong(self, obj: st.Inline.Strong) -> str:
+        return f"\\textbf{{{self.convert_element(obj.content)}}}"
+
+    def convert_strikeout(self, obj: st.Inline.Strikeout) -> str:
+        return f"\\sout{{{self.convert_element(obj.content)}}}"
+
+    def convert_superscript(self, obj: st.Inline.Superscript) -> str:
+        return f"\\textsuperscript{{{self.convert_element(obj.content)}}}"
+
+    def convert_subscript(self, obj: st.Inline.Subscript) -> str:
+        return f"\\textsubscript{{{self.convert_element(obj.content)}}}"
+
+    def convert_small_caps(self, obj: st.Inline.SmallCaps) -> str:
+        return f"\\textsc{{{self.convert_element(obj.content)}}}"
+
+    def convert_note(self, obj: st.Inline.Note) -> str:
+        return f"\\footnote{{{self.convert_element(obj.content)}}}"
+
+    def convert_link(self, obj: st.Inline.Link) -> str:
+        return f'\\href{{{obj.content[2].content[0]}}}{{{self.convert_element(obj.content[1])}}}'
+
+    def convert_image(self, obj: st.Inline.Image) -> str:
+        # Simple include graphics in Latex has no alt text, this is skipped
+        return f'\\includegraphics[width=0.25\linewidth]{{{obj.content[2].content[0]}}}'
+
+    def convert_code(self, obj: st.Inline.Code) -> str:
+        return f"\\verb|{obj.content[1]}|"
+
+    def convert_raw_inline(self, obj: st.Inline.RawInline) -> str:
+        return f"\\verb|{obj.content[1]}|"
+
+    def convert_horizontal_rule(self, obj: st.Block.HorizontalRule) -> str:
+        return "noindent\makebox[\linewidth]{\\rule{\paperwidth}{0.4pt}}"
+
+    def convert_plain(self, obj: st.Block.Plain) -> str:
+        block_content = "\n".join(obj.content)
+        return f"\\begin{{verbatim}}\n{block_content}\n\\end{{verbatim}}\n"
+
+    def convert_para(self, obj: st.Block.Para) -> str:
+        return self.convert_element(obj.content)
+
+    def convert_block_quote(self, obj: st.Block.BlockQuote) -> str:
+        return f"\\begin{{quote}}\n{self.convert_element(obj.content)}\n\end{{quote}}\n"
+
+    def convert_code_block(self, obj: st.Block.CodeBlock) -> str:
+        block_content = "\n".join(obj.content)
+        return f"\\begin{{verbatim}}\n{block_content}\n\\end{{verbatim}}\n"
+
+    def convert_raw_block(self, obj: st.Block.RawBlock) -> str:
+        # not supported in Latex
+        pass
+
+    # TODO
+    def convert_header(self, obj: st.Block.Header) -> str:
+        return f"\n{'=' * obj.content[0] + ' ' + self.convert_element(obj.content[2])}\n"
+
+    # TODO
+    def convert_table(self, obj: st.Block.Table) -> str:
+        align_dict = {"AlignLeft": "left", "AlignRight": "right", "AlignCenter": "center", "AlignDefault": "auto"}
+        columns = len(obj.content[2])
+        align = [i[0] for i in obj.content[2]]
+        align = [align_dict[i] for i in align]
+        rows = obj.content[3][2] + obj.content[4][3]
+        cells = [(self.convert_element(cell[4]) for cell in row[1]) for row in rows]
+        cells_in_table = ("[" + i + "],\n" for i in cells)
+        return f"""#align(center)[#table(
+                    columns: {columns},
+                    align: (col, row) => ({', '.join(align)}).at(col),
+                    inset: 6pt,
+                    {(cell for cell in cells_in_table).join("")}
+                """
+
+    def convert_bullet_list(self, obj: st.Block.BulletList, nesting: int = 0) -> str:
+        rows_txt = "\\begin{itemize}\n"
+        for i in obj.content[0]:
+            if isinstance(i, st.Block.BulletList):
+                rows_txt += f"{self.convert_bullet_list(i, nesting + 1)}"
+            elif isinstance(i, st.Block.OrderedList):
+                rows_txt += f"{self.convert_ordered_list(i, nesting + 1)}"
+            elif isinstance(i, st.Block.Plain) or isinstance(i, st.Block.Para):
+                rows_txt += f"\item {self.convert_element(i.content)}\n"
+            else:
+                rows_txt += f"\item {self.convert_element(i)}\n"
+        rows_txt += "\\end{itemize}\n"
+        return rows_txt
+
+    def convert_ordered_list(self, obj: st.Block.OrderedList, nesting: int = 0) -> str:
+        # list attributes are not supported
+        rows_txt = "\\begin{enumerate}\n"
+        for i in obj.content[1][0]:
+            if isinstance(i, st.Block.BulletList):
+                rows_txt += f"{self.convert_bullet_list(i, nesting + 1)}"
+            elif isinstance(i, st.Block.OrderedList):
+                rows_txt += f"{self.convert_ordered_list(i, nesting + 1)}"
+            elif isinstance(i, st.Block.Plain) or isinstance(i, st.Block.Para):
+                rows_txt += f"\item {self.convert_element(i.content)}\n"
+            else:
+                rows_txt += f"\item {self.convert_element(i)}\n"
+        rows_txt += "\\end{enumerate}\n"
+        return rows_txt
---
+++ tests/writers/latex_test.py
@@ -0,0 +1,113 @@
+import markup_converter.structure as ast
+from markup_converter.writers.latex_writer import LatexWriter
+
+
+def test_empty_document():
+    doc = ast.Document()
+    writer = LatexWriter(doc)
+    assert writer.write() == ""
+
+
+def test_convert_space():
+    doc = ast.Document(blocks=[ast.Inline.Space()])
+    writer = LatexWriter(doc)
+    assert writer.write() == " "
+
+
+def test_convert_soft_break():
+    doc = ast.Document(blocks=[ast.Inline.SoftBreak()])
+    writer = LatexWriter(doc)
+    assert writer.write() == "\\"
+
+
+def test_convert_link():
+    doc = ast.Document(blocks=[
+        ast.Inline.Link(
+            [
+                ast.Content.Attr(["", [], []]),
+                [ast.Inline.Str("Hello")],
+                ast.Content.Target(["http://example.com", ""]),
+            ]
+        )
+    ])
+    writer = LatexWriter(doc)
+    assert writer.write() == "\href{http://example.com}{Hello}"
+
+
+def test_convert_code():
+    doc = ast.Document(blocks=[
+        ast.Inline.Code(
+            [
+                ast.Content.Attr(["", [], []]),
+                "print('Hello, World!')",
+            ]
+        )
+    ])
+    writer = LatexWriter(doc)
+    assert writer.write() == "\\verb|print('Hello, World!')|"
+
+
+def test_block_quote():
+    doc = ast.Document(blocks=[
+        ast.Block.BlockQuote(
+            [   
+                ast.Block.Para(
+                [
+                    ast.Inline.Str("Hello,"),
+                    ast.Inline.Space(),
+                    ast.Inline.Str("world!"),
+                    ast.Inline.SoftBreak(),
+                    ast.Inline.Str("Hi")
+                ]
+            ),
+            ]
+        )
+    ])
+    writer = LatexWriter(doc)
+    assert (
+        writer.write()
+        == """\\begin{quote}
+Hello, world!\\Hi
+\end{quote}
+"""
+    )
+
+def test_list():
+    doc = ast.Document(
+        blocks=[
+            ast.Block.OrderedList(
+                [
+                    ast.Content.ListAttributes(
+                        [1, ast.Enum.ListNumberStyle("Decimal"), ast.Enum.ListNumberDelim("Period")]
+                    ),
+                    [
+                        [
+                            ast.Block.Para([ast.Inline.Str("This is a list item.")]),
+                            ast.Block.Para([ast.Inline.Str("This is another list item.")]),
+                            ast.Block.BulletList(
+                                [
+                                    [
+                                        ast.Block.Plain([ast.Inline.Str("This is a nested list item.")]),
+                                        ast.Block.Plain([ast.Inline.Str("This is another nested list item.")]),
+                                    ]
+                                ]
+                            ),
+                        ]
+                    ],
+                ]
+            )
+        ]
+    )
+    writer = LatexWriter(doc)
+    assert (
+        writer.write()
+        == """\\begin{enumerate}
+\item This is a list item.
+\item This is another list item.
+\\begin{itemize}
+\item This is a nested list item.
+\item This is another nested list item.
+\end{itemize}
+\end{enumerate}
+"""
+    )
